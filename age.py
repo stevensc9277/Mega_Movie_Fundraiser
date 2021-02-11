6a27b9633e1c40ced381733efc25bbae3b49a4a9
Caleb Stevens <stevensc9277@masseyhigh.school.nz>
2021-02-11 22:40:36 +0000

    has function that verifies the age of the user (12 <= age <= 130)

--- age.py
+++ age.py
@@ -1,14 +1,13 @@
 # Number checker used to check user's age
-def num_check(question, low, high):
-  error = "Please enter a valid number"
+def num_check(question, low, high, error):
   while True:
     try:
       response = int(input(question))
       if response < low:
-        print(error)
+        print("Please enter an integer that is more than (or equal to) {}".format(low))
         print()
       elif response > high:
-        print(error)
+        print("Please enter an integer that is less than (or equal to) {}".format(high))
         print()
       else:
         print()
@@ -18,4 +17,4 @@ def num_check(question, low, high):
       print()
 
 for i in range(0, 3):
-  age_verify = num_check("How old are you? ", 12, 70)
+  age_verify = num_check("How old are you? ", 12, 130, "Please enter a valid age between 12 and 130")
