bc90f8b165608e5bc7e1f13a02e7ce1248db6a6a
Caleb Stevens <stevensc9277@masseyhigh.school.nz>
2021-02-11 23:04:16 +0000

    shows user how many tickets are left after ending program

--- 02_ticket_loop_v1.py
+++ 02_ticket_loop_v1.py
@@ -2,15 +2,16 @@
 
 # initialise loop so that it runs at least once
 name = ""
-count = 1
+count = 0
 max_tickets = 5
-tickets_left = 5
 while name != "xxx" and count <= max_tickets:
   # get details
-  print("You have {} seats left".format(tickets_left))
+  print("You have {} seats left".format(max_tickets - count))
   name = input("Name: ")
   count += 1
-  tickets_left -= 1
-
+ 
   print()
-print("You have sold all available tickets")
+if count == max_tickets:
+  print("You have sold all available tickets")
+else:
+  print("You have {} tickets left".format(max_tickets - count + 1))
