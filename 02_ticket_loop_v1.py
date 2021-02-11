0b56e6f590729f1f9be2ebbce2a9f0741425fd20
Caleb Stevens <stevensc9277@masseyhigh.school.nz>
2021-02-11 00:18:34 +0000

    Shows user how many seats are left after getting a ticket

--- 02_ticket_loop_v1.py
+++ 02_ticket_loop_v1.py
@@ -4,8 +4,12 @@
 name = ""
 count = 1
 max_tickets = 5
+tickets_left = 5
 while name != "xxx" and count <= max_tickets:
   # get details
   name = input("Name: ")
   count += 1
+  tickets_left -= 1
+  print("You have {} seats left".format(tickets_left))
+  print()
 print("You have sold all available tickets")
