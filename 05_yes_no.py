92e985565227d95e7ba741d71e829cf8ed237447
Caleb Stevens <stevensc9277@masseyhigh.school.nz>
2021-02-18 22:51:07 +0000

    yes/no string checker

---
+++ 05_yes_no.py
@@ -0,0 +1,19 @@
+def yes_no(question):
+  error = "Please answer yes / no"
+
+  while True:
+
+    # ask question and put response in lowercase
+    response = input(question).lower()
+    if response == "yes" or response == "y":
+      return "yes"
+
+    elif response == "no" or response == "n":
+      return "no"
+
+    else:
+      print(error) 
+
+for item in range(0, 6):
+  want_snack = yes_no("Do you want snacks? ")
+  print("Answer OK, you said:", want_snack)
