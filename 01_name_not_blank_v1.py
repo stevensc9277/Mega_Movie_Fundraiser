859f1b686ed5b1d97ec2f626409395e8bca93072
Caleb Stevens <stevensc9277@masseyhigh.school.nz>
2021-02-18 23:13:55 +0000

    function does not accept a name with numbers

--- 01_name_not_blank_v1.py
+++ 01_name_not_blank_v1.py
@@ -2,10 +2,10 @@
 def name_check(question):
   while True:
     response = input(question)
-    if response.isnumeric != False: 
+    if response.isnumeric() != False: 
       return response
     else:
-      print("Sorry - this can't be blank, please enter your name")
+      print("Sorry - this can't be blank and we also do not accept numbers with your name. \nPlease enter your name")
       print()
 
 user = name_check("Name: ")
