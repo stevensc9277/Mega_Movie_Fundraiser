be9f6a1a00644178177a18cc475f6fd72682a8ef
Caleb Stevens <stevensc9277@masseyhigh.school.nz>
2021-02-18 00:05:49 +0000

    integrated ticket price code

--- main.py
+++ main.py
@@ -34,35 +34,53 @@ def num_check(question):
 
 # initialise loop so that it runs at least once
 name = ""
-count = 0
+ticket_count = 0
+profit = 0
+ticket_sales = 0
 max_tickets = 5
-while name != "xxx" and count <= max_tickets:
-  # get details
-  print("You have {} seats left".format(max_tickets - count))
-  # ask for name
+while name != "xxx" and ticket_count <= max_tickets:
+  # tells user how many seats are left
+  if ticket_count < max_tickets - 1:
+    print("You have {} seats left".format(max_tickets - ticket_count))
+  
+  else:
+    print("*** There is ONE seat left!! ***")
+  # get details 
   name = name_check("Name: ")
   
 # End the loop if exit code is entered
   if name == "xxx":
     break
-  count += 1
-
-  print()
-if count == max_tickets:
-  print("You have sold all available tickets")
-else:
-  print("You have {} tickets left".format(max_tickets - count + 1))
-
-
-  age = num_check("How old are you? ")
+  
   # Check that age is valid...
+  age = num_check("How old are you? ")
   if age < 12:
     print("Sorry, you are too young for this movie")
     continue
   elif age > 130:
     print("That is very old - it looks like a mistake")
     continue
+  ticket_count += 1
 
+  if age < 16:
+    ticket_price = 7.5
+
+  elif  age < 65:
+    ticket_price = 10.5
+
+  else:
+    ticket_price = 6.5
+  print()
+
+  
+  ticket_count += 1
+  ticket_sales += ticket_price
+if ticket_count == max_tickets:
+  print("You have sold all available tickets")
+else:
+  print("You have {} tickets left".format(max_tickets - ticket_count + 1))
+
+  
 # Ask user what snack they want and how many portions
 # Ask for the payment method (cash/credit)
 # Credit payment incurs a 5% surcharge which is not included in the movie company's profits (goes to credit company)
