d15df377f4a4857044f2e52977ca6e5a50d4c8dc
Caleb Stevens <stevensc9277@masseyhigh.school.nz>
2021-02-21 21:54:10 +0000

    forgot to add commas after lists

--- 06_string_check_v2.py
+++ 06_string_check_v2.py
@@ -1,10 +1,10 @@
 # valid snacks holds list of all snacks
 # Each item in valid snacks is a list with valid options for each snack < full name, letter code (a - e), and possibly abbreviations, etc
 
-valid_snacks =[
-  ["popcorn", "p", "corn", "a"]
-  ["M&M's", "m&m's", "mms", "m", "b"]
-  ["pita chips", "chips", "pc", "pita", "c"]
+valid_snacks = [
+  ["popcorn", "p", "corn", "a"],
+  ["M&M's", "m&m's", "mms", "m", "b"],
+  ["pita chips", "chips", "pc", "pita", "c"],
   ["water", "w", "d"]
 ]
 
