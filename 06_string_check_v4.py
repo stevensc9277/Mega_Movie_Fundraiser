5ad1e9d6321c06dde9d7dc43c659a5ff024082ce
Caleb Stevens <stevensc9277@masseyhigh.school.nz>
2021-02-22 20:55:36 +0000

    confirm if user want snack

---
+++ 06_string_check_v4.py
@@ -0,0 +1,14 @@
+def confirm():
+  response = input("Do oyu want to order snacks? ").lower()
+  if response not in yes_no:
+    return "Please enter a valid response"
+  else:
+    return response
+
+
+
+yes_no = [
+  ["yes", "y"],
+  ["no", "n"]
+]
+snack
--- 06_string_func.py
+++ 06_string_func.py
@@ -20,6 +20,8 @@ def string_check(choice, options):
     return chosen
   
   else:
+    print("Please enter a valid option")
+    print()
     return "invalid choice"
 
 # valid snacks holds list of all snacks
@@ -37,10 +39,42 @@ yes_no = [
   ["no", "n"]
 ]
 
+# holds snack order for a single user
+snack_order = []
 check_snack = "invalid choice"
 while check_snack == "invalid choice":
   want_snack = input("Do you want to order snacks? ").lower()
   check_snack = string_check(want_snack, yes_no)
+
+# if they say yes, ask what snacks they want (and add to our snack list)
+if check_snack == "Yes":
+  
+  desired_snack = ""
+  while desired_snack != "xxx":
+    # ask user for desired snack and put it in lowercase
+    desired_snack = input("Snack: ").lower()
+    
+    if desired_snack == "xxx":
+      break
+    
+    snack_choice = string_check(desired_snack, valid_snacks)
+    print("Snack choice: ", snack_choice)
+
+    # add snack to list...
+    # check that snack is not the exit code before adding
+    if snack_choice != "xxx" and snack_choice != "invalid choice":
+      snack_order.append(snack_choice)
+
+# show snack orders
+print()
+if len(snack_order) ==  0:
+  print("Snacks ordered: None")
+
+else:
+  print("Snacks ordered: ")
+  
+  for i in snack_order:
+    print(i)
 
 # loop three times to make testing quicker
 for item in range(0, 6):
