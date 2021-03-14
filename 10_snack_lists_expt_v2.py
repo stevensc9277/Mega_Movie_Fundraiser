f6d14c10db650b45c4fcbab7c23d42da29c6e8f1
Caleb Stevens <stevensc9277@masseyhigh.school.nz>
2021-03-14 21:54:30 +0000

    working on snack list

---
+++ 10_snack_lists_expt_v2.py
@@ -0,0 +1,25 @@
+# initialise snack lists...
+
+names = ['Rangi', 'Manaia', 'Talia', 'Arihi', 'Fetu']
+
+popcorn = []
+mms = []
+pita_chips = [] 
+water = [] 
+orange_juice = []
+
+snack_lists = [popcorn, mms, pita_chips, water, orange_juice]
+
+snack_menu_dict = {
+  'Popcorn': popcorn,
+  'Water': water,
+  'Pita Chips': pita_chips,
+  'M&Ms': mms,
+  'Orange Juice': orange_juice
+}
+
+test_data = [
+  [[2, 'Popcorn'], [1, 'Pita Chips'], [1, 'Orange Juice']],
+  [[1, 'Pita Chips'], [1, 'Orange Juice'], [2, 'M&Ms']],
+  [[1, 'Water']]
+]
