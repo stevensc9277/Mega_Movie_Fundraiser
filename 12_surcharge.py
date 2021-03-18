3873704db37a22cc9fe8148da33375e5cd1d48d5
Caleb Stevens <stevensc9277@masseyhigh.school.nz>
2021-03-18 00:19:26 +0000

    surcharge testing

---
+++ 12_surcharge.py
@@ -0,0 +1,54 @@
+import pandas
+
+
+# Function goes here
+# Warning: The response is returned in Title Case
+def string_check(choice, options):
+  for var_list in options:
+
+    # if the snack is in one of the lists, return the full name
+    if choice in var_list:
+
+      # Get full snack and put it in title case so it looks nice when outputted
+      chosen = var_list[0].title()
+      is_valid = "yes"
+      break
+    
+    # if the chosen option is not valid, set is_valid to no
+    else:
+      is_valid = "no"
+  
+  # if snack is not ok - ask question again.
+  if is_valid == "yes":
+    return chosen
+  
+  else:
+    print("Please enter a valid option")
+    print()
+    return "invalid choice"
+
+# Main routine
+pay_method = [
+  ["cash", "ca"]
+["credit", "cr"]
+]
+
+# loop until exit code
+name = ""
+while name != "xxx":
+  name = input("Name: ")
+  if name == "xxx":
+    break
+
+  how_pay = "invalid choice"
+  while how_pay == "invalid choice":
+    how_pay = input("Please choose a payment method (cash / credit)? ")
+  
+  # Ask for subtotal (for testing purposes)
+  subtotal = float(input("Sub total? $"))
+  
+  if how_pay == "Credit":
+    surcharge = 0.05 * subtotal
+
+  else:
+    surcharge = 0
